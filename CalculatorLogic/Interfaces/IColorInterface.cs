a157be42c77bc80874179f8ccd5ec6e02df6df92
abdelhady24 <abdelhady18@gmail.com>
2020-01-07 23:39:28 +0200

    Sprint 2 requirments

---
+++ CalculatorLogic/Interfaces/IColorInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLogic.Interfaces
+{
+   public interface IColorInterface
+    {
+        ConsoleColor SetColor(int colorEnum);
+        //Create an enum  for color 
+        // Dont set color in main function. Set color after return result from operation function 
+    }
+}
---
+++ CalculatorLogic/Interfaces/ILanguageInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CalculatorLogic.Interfaces
+{
+    public interface ILanguageInterface
+    {
+        CultureInfo SetLanguage(int languageEnum);
+        // create resource file for every language 
+        // configure resource files to correct language 
+        //print string messages from localization according to every language from resouce file
+    }
+}
